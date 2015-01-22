413ff692c21d1b0b918dd94ae7a9052c3ddef688
Scott <kettlescott@gmail.com>
2015-01-23 10:29:46 +1300

    Update PathSum.java

--- PathSum.java
+++ PathSum.java
@@ -4,17 +4,7 @@ import java.util.Stack;
 
 public class PathSum {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-          String [] data = {"1" ,"2"};
-          TreeNode root = LeetCodeTree.createTree(data) ;
-          PathSum p = new PathSum ();          
-          boolean bflag = p.hasPathSum(root, 3) ;          
-          System.out.println(bflag);
-	}
+
 	
 	 public boolean hasPathSum(TreeNode root, int sum) {
 	    Stack <TreeNode> stack = new Stack<> ();	    
