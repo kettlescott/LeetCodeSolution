f18fe5885d1eb0856c8831552fd6fca19f787a49
Scott <kettlescott@gmail.com>
2015-01-23 10:03:06 +1300

    Update FlattentoLinked.java

--- FlattentoLinked.java
+++ FlattentoLinked.java
@@ -5,9 +5,18 @@ public class FlattentoLinked {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+     public TreeNode flatten(TreeNode root) {
+        if (root == null) return null ;
+        TreeNode leftNode  = flatten (root.left) ;
+        TreeNode rightNode = flatten (root.right) ;            
+        root.right = leftNode ;
+        root.left = null ;
+        TreeNode t = root ;        	
+    	while (t != null && t.right != null)
+    		 t = t.right ;        	
+    	if (t != null)
+    		t.right = rightNode ;                 
+        return root;        
+    }
 
 }
