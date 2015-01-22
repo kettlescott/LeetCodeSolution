6b66e688307e0ec226e224c17fb9a98ada904b0d
Scott <kettlescott@gmail.com>
2015-01-23 10:49:30 +1300

    Update WordCount.java

--- WordCount.java
+++ WordCount.java
@@ -2,29 +2,16 @@ package com.scott.leetcode;
 
 public class WordCount {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String words="hello this is a word,how are you? ";
-       System.out.println(calWordCount(words));
-	}
-	
-	
-	
 	public static int calWordCount(String words){
 		int count = 0;
 		boolean preHasSpace=true;
 		char [] chs=words.toCharArray();
-		
 		for (int i = 0; i < chs.length; ++i){
 			if (Character.isAlphabetic(chs[i])||Character.isDigit(chs[i])){
 				if (preHasSpace){
 					count++;
 					preHasSpace=false;
 				}
-				
 			}else{
 				preHasSpace=true;
 			}
