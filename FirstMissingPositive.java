d6e5f4f2a811ea552474b432d32be254756baae1
Scott <kettlescott@gmail.com>
2015-01-23 10:10:28 +1300

    Update FirstMissingPositive.java

--- FirstMissingPositive.java
+++ FirstMissingPositive.java
@@ -2,55 +2,7 @@ package com.scott.leetcode;
 
 public class FirstMissingPositive {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		FirstMissingPositive  m = new FirstMissingPositive ();
-		int [] data = {2, 1} ;
-		int r = m.firstMissingPositive(data) ;
-//		System.out.println(r);
-		System.out.println(m.firstMissingPositive_m(data));
-	}
 
-	
-	
-	
-	 public int solution(int[] A) {
-	        // write your code in Java SE 8
-	    int [] map = new int [A.length] ;
-	    for (int i = 0 ; i < A.length ; ++i) {
-	    	int target = A[i];
-	    	if (target <= A.length){
-	    		map[target - 1] = 1 ;
-	    	}
-	    }
-	    for (int c : map) {
-	    	if ( c == 0)  return 0 ;
-	    }
-	    
-	   return 1 ;
-	 }
-	
-	
-	
-	 public int solution(String S) {
-	        // write your code in Java SE 8
-	       int count = 0 ;
-	       for (Character c : S.toCharArray()) {
-	    	   if (c == '(') {
-	    		   count++;
-	    	   } else {
-	    		   if (count == 0) return 0 ;
-	    		   count--;
-	    	   }
-	       }	 
-	       return count == 0 ?  1 : 0 ;
-	 }
-	
-	
-	
 	public int firstMissingPositive(int[] A) {		
 		for (int i = 0 ; i < A.length ; ++i) {
 		   int target = A[i] ;
