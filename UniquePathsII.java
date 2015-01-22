80f31be928d6da38896f503b395f9eed26a22c5b
Scott <kettlescott@gmail.com>
2015-01-23 10:45:25 +1300

    Update UniquePathsII.java

--- UniquePathsII.java
+++ UniquePathsII.java
@@ -2,33 +2,6 @@ package com.scott.leetcode;
 
 public class UniquePathsII {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        int [][] matrix = {
-        		  { 0,0,0 } ,
-        		  { 0,1,0 },
-        		  { 0,0,1 }
-        } ;
-        UniquePathsII p = new UniquePathsII ();
-//        System.out.println(p.uniquePathsWithObstacles(matrix));
-        int [] array = {100 , 1} ;
-        System.out.println(p.getMax(array, 0, array.length - 1));
-	}
-	
-	
-	public int getMax (int [] array , int L , int R) {
-		if (L == R) {
-			return array[L];
-		}
-		int M = (L + R) / 2 ;
-		int left = getMax (array, L ,M) ;
-		int right = getMax (array, M + 1 ,R) ;
-		return left >= right ? left : right;
-		 			
-	}
 	
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
     	int m = obstacleGrid.length ;
