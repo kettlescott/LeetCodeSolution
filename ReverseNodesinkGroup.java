7d77a661d7abc89716b68d0c0a51923a94b019aa
Scott <kettlescott@gmail.com>
2015-01-23 16:23:31 +1300

    Update ReverseNodesinkGroup.java

--- ReverseNodesinkGroup.java
+++ ReverseNodesinkGroup.java
@@ -1,50 +1,24 @@
 public class ReverseNodesinkGroup {
+    
+    /*
+       Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ReverseNodesinkGroup r = new ReverseNodesinkGroup () ;
-		int [] data = {1,2,3} ;
-		ListNode head = ListNode.createList(data) ;
-		head = r.reverseKGroup_solutionII(head, 2) ;	
-		ListNode p = head ;
-		 
-		while (p != null) {
-			System.out.print(p.val + " ");
-			p = p.next ;
-		}
-	}
-	
-	public ListNode reverseKGroup_solutionII(ListNode head, int k)  {
-		ListNode dummyNode = new ListNode (1);
-		ListNode p = head ,back = dummyNode  ,tail = null;
-		dummyNode.next = head ;
-		ListNode n = null ;
-		int step = 1 ;
-		while (p != null && k != 0) {
-			ListNode next = p.next ;
-			if (step % k == 0) {
-				ListNode tmp = back.next ;
-				back.next = next;
-				System.out.println(next.val);
-				if (n == null){
-					n = p ;
-				}
-				while (n != null) {
-					System.out.println(n.val);
-					n = n.next ;
-				}
-		   	}
-		   	p.next = tail ;
-		   	tail = p ;
-		   	p = next ;
-		   	step++ ;
-		}
-		return n ;
-	}
-	
-	
-	
-	//1->2->3->4->5 , 2->1->4->3->5
+       If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+
+       You may not alter the values in the nodes, only nodes itself may be changed.
+
+       Only constant memory is allowed.
+
+       For example,
+       Given this linked list: 1->2->3->4->5
+
+       For k = 2, you should return: 2->1->4->3->5
+
+       For k = 3, you should return: 3->2->1->4->5
+  
+    */
+
+
     public ListNode reverseKGroup(ListNode head, int k) {
        ListNode dummyHead = new ListNode(-1);
        dummyHead.next = head ;
