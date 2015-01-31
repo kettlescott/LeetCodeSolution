6b72a991f051b7cce30b598d6a0b398c2fc4e210
Scott <kettlescott@gmail.com>
2015-01-31 22:58:22 +1300

    Update WildcardMatching.java

--- WildcardMatching.java
+++ WildcardMatching.java
@@ -72,5 +72,32 @@ isMatch("aab", "c*a*b") → false
     	}
     	return 0 ;
     }
+    
+    public boolean isMatch_iter(String s, String p) {
+		 int pl = 0;
+		 for (int i = 0; i < p.length(); i++) {
+		    if (p.charAt(i) != '*') {
+		           pl++;
+		      }
+		 }
+		if (pl > s.length()) return false; 
+	   boolean [][] dp = new boolean[s.length() + 1][p.length() + 1] ;
+	   dp[0][0] = true ;
+	   for (int i = 1 ; i <= p.length() ; ++i) {
+		   if (dp[0][i - 1] && p.charAt(i - 1) == '*') dp[0][i] = true ;
+	   }	  
+	   for (int i = 1 ; i <= s.length()  ; ++i) {		   
+		   for (int j = 1 ; j <= p.length() ; ++j) {
+			   if (p.charAt(j - 1) == '?' || s.charAt( i - 1) == p.charAt(j - 1)) {
+				  dp [i][j] = dp[i -1][j - 1];
+			   } else if (p.charAt(j - 1) == '*') {
+				   dp [i][j] = dp[i -1][j] || dp[i][j - 1]; 
+			   } else {				   
+				   dp [i][j] = false ;
+			   }
+		   }
+	   }	   
+	   return dp[s.length()][p.length()] ;	 
+	 }
 
 }
