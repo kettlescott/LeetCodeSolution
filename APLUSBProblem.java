ebfde3140a66f088b256b9442fea382d03b00900
Scott <kettlescott@gmail.com>
2015-02-12 09:22:17 +1300

    Update APLUSBProblem.java

--- APLUSBProblem.java
+++ APLUSBProblem.java
@@ -20,9 +20,8 @@ Are a and b both 32-bit integers?
 	*/
 	 public int aplusb(int a, int b) {
 	        // Click submit, you will get Accepted!
-	      if (b > a) return aplusb (b,a);
-	      if (a == b) return 2 * a ;	      
-	      return (a*a - b*b) / (a- b) ;
+		  if (a == b) return 2 * a ;	
+	      return (a * a - b * b) / (a- b) ;
 	 }
 
 }
