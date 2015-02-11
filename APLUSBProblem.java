968c7691c5cda02cc254fd7c9f2209e1546a3fb7
Scott <kettlescott@gmail.com>
2015-02-11 15:44:59 +1300

    Update APLUSBProblem.java

--- APLUSBProblem.java
+++ APLUSBProblem.java
@@ -3,12 +3,26 @@ package com.scott;
 public class APLUSBProblem {
 
 	
-	
+	/*
+	For given numbers a and b in function aplusb, return the sum of them.
+
+Note
+You don't need to parse the input and output. Just calculate and return.
+
+Example
+If a=1 and b=2 return 3
+
+Challenge
+Can you do it with out + operation?
+
+Clarification
+Are a and b both 32-bit integers?
+	*/
 	 public int aplusb(int a, int b) {
 	        // Click submit, you will get Accepted!
 	      if (b > a) return aplusb (b,a);
 	      if (a == b) return 2 * a ;	      
-	      return( a*a - b*b) / (a- b) ;
+	      return (a*a - b*b) / (a- b) ;
 	 }
 
 }
